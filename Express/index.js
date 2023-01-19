68f0100221e2248884d66a7b40eaa008c923a643
Abhinav <abhinav@AVs-Mac.local>
2023-01-19 23:26:59 +0530

    [18th Jan 2023]params, query params.

--- Express/index.js
+++ Express/index.js
@@ -2,11 +2,24 @@ const express = require('express');
 // console.log(express);
 const app = express();
 
+/*
 app.get('/', (req, res) => {
+    // console.log(req.headers);
+    // console.log(req.path);
+    // text/html
     res.send('Hi this is my server');
 })
 
 app.get('/about', (req, res) => {
+    // console.log(req.path);
+    // console.log('hostname', req.hostname);
+    // console.log('ip',req.ip);
+    // console.log('protocol', req.protocol);
+
+    console.log('params', req.params);
+    console.log('query', req.query);
+    console.log('body', req.body);
+
     res.send('<h2>About Page</h2>');
     // res.end('<h2>About Page</h2>');
 })
@@ -15,6 +28,56 @@ app.get('/data', (req, res) => {
     res.type('application/json')
     res.send({name: [1,2,3]})
 })
+
+const personListIndia = []
+const personListUS = []
+
+
+app.get('/personInfo/:personid/:region', (req, res) => {
+    console.log('params', req.params);
+    res.send('This is person Info ');
+})
+*/
+
+const productsList = [
+    {
+        id: '1',
+        type: 'Clothing',
+        name: 'Shirt'
+    },
+    {
+        id: '2',
+        type: 'accessories',
+        name: 'cover'
+    },
+    {
+        id: '3',
+        type: 'Electronic',
+        name: 'Mobile'
+    }
+]
+
+app.get('/products', (req, res) => {
+    console.log(req.query);
+    res.send({productsList})
+})
+
+app.get('/', (req, res) => {
+    res.send('<div>this is me</div>')
+})
+
+app.get('/products/:id', (req, res) => {
+    // req.params wil be an object 
+    console.log(req.params);
+    const product = productsList.find((product) => product.id === req.params.id);
+    if(product){
+        res.send(product)
+    } else{
+        res.send('Product not found!!')
+    }
+    console.log(product);
+})
+
 
 const PORT = 4050
 app.listen(PORT, () => {
